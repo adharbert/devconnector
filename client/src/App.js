33828055089eb8039dc2c76b17087eae666e1a3a
Andy Harbert <adharbert@gmail.com>
2018-11-22 16:57:38 -0500

    Adding authentication logic with jwt tokens and actions.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -11,7 +14,15 @@ import store from './store';
 
 import './App.css';
 
-
+// Check for token
+if (localStorage.jwtToken) {
+  // set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 
 class App extends Component {
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { GET_ERROR } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+import { GET_ERROR, SET_CURRENT_USER } from './types';
 
 
 export const registerUser = (userData, history) => dispatch => {
@@ -12,3 +14,34 @@ export const registerUser = (userData, history) => dispatch => {
             })
         );
 };
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+    axios.post('/api/users/login', userData)
+         .then(res => {
+            // Save to localStorage
+            const { token } = res.data;
+            // Set token to ls
+            localStorage.setItem('jwtToken', token);
+            // Set token to Auth Header
+            setAuthToken(token);
+            // Decode token to get user data
+            const decoded = jwt_decode(token);
+            // set current user
+            dispatch(setCurrentUser(decoded))
+         })
+         .catch(err => 
+            dispatch({
+                type: GET_ERROR,
+                payload: err.response.data
+            }))
+}
+
+
+// Set logged in user
+export const setCurrentUser = (decoded) =>  {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
