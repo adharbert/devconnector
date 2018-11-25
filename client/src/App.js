f50dfa307edb2ea04f75469a47c63713bdb7be9d
Andy Harbert <adharbert@gmail.com>
2018-11-24 22:57:46 -0500

    Adding profile dashboard.  Creating profile action and reducer.

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import { Provider } from 'react-redux';
+import Dashboard from './components/dashboard/Dashboard';
 import store from './store';
 
 
@@ -28,7 +30,8 @@ if (localStorage.jwtToken) {
   if(decoded.exp < currentTime) {
     // Logout the user
     store.dispatch(logoutUser());
-    // TODO: Clear current Profile
+    //  Clear current Profile
+    store.dispatch(clearCurrentProfile())
     // Redirect to login
     window.location.href = '/login';
   }
@@ -47,6 +50,7 @@ class App extends Component {
               <div className="container">
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
+                <Route exact path="/dashboard" component={Dashboard} />
               </div>
             <Footer />
           </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
-
+import { clearCurrentProfile } from '../../actions/profileActions';
 
 class Navbar extends Component {
 
     onLogoutClick(e) {
         e.preventDefault();
+        this.props.clearCurrentProfile();
         this.props.logoutUser();
     }
 
@@ -31,7 +32,7 @@ class Navbar extends Component {
         const authLinks = (        
             <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                    <a  href="" 
+                    <div 
                         onClick={this.onLogoutClick.bind(this)} 
                         className="nav-link">
                         <img src={user.avatar} 
@@ -40,7 +41,7 @@ class Navbar extends Component {
                              style={{ width: '25px', marginRight: '5px' }} 
                              title="You must have a Gravatar connected to your email to display an image" />
                         Logout
-                    </a>
+                    </div>
                 </li>
             </ul>
         );
@@ -78,4 +79,4 @@ Navbar.propTypes = {
     auth: state.auth
   });
   
-  export default connect(mapStateToProps, { logoutUser })(Navbar);
+  export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })(Navbar);
