4cbbcfb5ac66288a464246f8862a8fe03d8903f6
adharbert <adharbert@gmail.com>
2018-11-28 22:27:02 -0500

    Adding dashboard profile actions.

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
 
 
 // Get current profile
@@ -30,6 +30,29 @@ export const createProfile = (profileData, history) => dispatch => {
              payload: err.response.data
          }))
 }
+
+
+
+// Delete account & profile
+export const deleteAccount = () => dispatch => {
+    if(window.confirm('Are you sure? This can NOT be undone!')) {
+        axios.delete('/api/profile')
+             .then(res => 
+                dispatch({
+                    type: SET_CURRENT_USER,
+                    payload: {}
+                })
+            )
+            .catch(err => 
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                })
+            );
+    }
+};
+
+
 
 
 export const setProfileLoading = () => {
