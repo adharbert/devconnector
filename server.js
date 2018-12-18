0fcdb08d7f48d78d01cea88ea7d0f0bc373ed736
adharbert <adharbert@gmail.com>
2018-12-17 23:15:42 -0500

    Prepared for deploy

--- server.js
+++ server.js
@@ -19,18 +19,6 @@ const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
-// Server static assets if in production
-if(process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
-
-// configure app
-const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -46,6 +34,20 @@ require('./config/passport')(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+// configure app
+const app = express();
+
 
 const port = process.env.PORT || 5000;
 
